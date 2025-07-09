ac8038d3c0a0daab85055e78b1d3fccc56c5ca04
Nikhil Madhav <nikhil_madhav@gap.com>
2025-07-08 23:06:21 -0500

    move some tests around

--- apps/app1/tests/functional/Interactions.test.ts
+++
@@ -1,7 +0,0 @@
-import test from '@app1Base';
-
-test(`Verify Interactions Page`, { tag: '@Smoke'}, async ({ loginPage, interactionsPage, webActions }) => {
-    await loginPage.navigateToURL();
-    await webActions.clickByText('Interactions'); // Click on Interactions Icon identified via text selector
-    await webActions.clickByText('Droppable');
-    await interactionsPage.verifyDragandDrop();});
